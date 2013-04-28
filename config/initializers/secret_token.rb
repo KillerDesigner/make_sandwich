af8bb665e319863ee2338a7b759cc9aed3c396f6
Feather <lisafeather@gmail.com>
2013-04-28 13:16:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeSandwich::Application.config.secret_token = 'cb78613804de7f39701cfb9e8e11a120288580ec91db12651a51446befd3fcccf96fc1c09e928d97ce83c90228cc88d7373b4c05d9211b9ef9c8644f4cfa55d0'
