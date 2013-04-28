9ed9bce300e8d25e2d0176e391eca582477e13bb
Feather <lisafeather@gmail.com>
2013-04-28 16:14:54 -0700

    add image and name to user email

--- app/mailers/sandwichmaker_mailer.rb
+++ app/mailers/sandwichmaker_mailer.rb
@@ -3,7 +3,10 @@ class SandwichmakerMailer < ActionMailer::Base
 
   def confirm_order(user)
 	@user = user
-	#attachments["sandwich.jpg"] = File.read("#{Rails.root}/public/images/cut_sandwich.jpg")
+	attachments["cut_sandwich.jpg"] = File.read("#{Rails.root}/app/assets/images/cut_sandwich.jpg")
 	mail(:to => "#{user.name} <#{user.email}>", :subject => "Sandwich Request")
   end
 end
+
+#/Users/lisafeather/workspace/learn_stuff/make_sandwich/public/images/cut_sandwich.jpg
+#/Users/lisafeather/workspace/learn_stuff/make_sandwich/app/assets/images
