b938b93dcef55654bf16951d3d9fc9430b0cbd70
Feather <lisafeather@gmail.com>
2013-05-01 20:41:30 -0700

    more changes for heroku

--- app/mailers/sandwichmaker_mailer.rb
+++ app/mailers/sandwichmaker_mailer.rb
@@ -1,12 +1,12 @@
 class SandwichmakerMailer < ActionMailer::Base
-  default from: "featherknee+sandwich@gmail.com"
+  default from: "bestsandwichmaker@gmail.com"
 
   def confirm_order(sandwich_orders)
   	#@user = user
 	@sandwich_order = sandwich_orders
 	attachments["cut_sandwich.jpg"] = File.read("#{Rails.root}/app/assets/images/cut_sandwich.jpg")
 	#mail(:to => "#{user.name} <#{user.email}>", :subject => "Sandwich Request")
-	mail(:to => "lisafeather@gmail.com", :subject => "Sandwich Request")
+	mail(:to => "bestsandwichmaker@gmail.com", :subject => "Sandwich Request")
 	# make_sandwich@generalthings.com
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,15 +54,15 @@ MakeSandwich::Application.configure do
   config.action_mailer.delivery_method = :smtp
   # these setting will only work if the Bash environment vars have been set
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "www.gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "bestsandwichmaker@gmail.com",
+    password: "88sandwich"
   }
-
+  config.action_mailer.default_url_options = {host: "sandwich-master/heroku.com"}
   
   # Enable threaded mode
   # config.threadsafe!
