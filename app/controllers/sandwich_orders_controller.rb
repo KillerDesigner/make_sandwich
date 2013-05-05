92497c092a5dc2f3eb37b572ef82b3c36b8be4a8
Feather <lisafeather@gmail.com>
2013-05-05 14:08:55 -0700

    update update to send confirmation message

--- app/controllers/sandwich_orders_controller.rb
+++ app/controllers/sandwich_orders_controller.rb
@@ -23,11 +23,23 @@ class SandwichOrdersController < ApplicationController
     @sandwich_order = SandwichOrder.find(params[:id])
   end
 
+  def update
+    @sandwich_order = SandwichOrder.find(params[:id])
+
+    respond_to do |format|
+      if @sandwich_order.update_attributes(params[:sandwich_order])
+      	SandwichmakerMailer.confirm_order(@sandwich_order).deliver
+        format.html { redirect_to @sandwich_order, notice: 'Order was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @sandwich_order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def create
     @sandwich_order = SandwichOrder.new(params[:sandwich_order])
-    puts '******************************'
-    puts params[:sandwich_order]
-
     respond_to do |format|
       if @sandwich_order.save
         SandwichmakerMailer.confirm_order(@sandwich_order).deliver
--- app/mailers/sandwichmaker_mailer.rb
+++ app/mailers/sandwichmaker_mailer.rb
@@ -2,11 +2,11 @@ class SandwichmakerMailer < ActionMailer::Base
   default from: "bestsandwichmaker@gmail.com"
 
   def confirm_order(sandwich_orders)
-  	#@user = user
 	@sandwich_order = sandwich_orders
 	attachments["cut_sandwich.jpg"] = File.read("#{Rails.root}/app/assets/images/cut_sandwich.jpg")
 	#mail(:to => "#{user.name} <#{user.email}>", :subject => "Sandwich Request")
 	mail(:to => "bestsandwichmaker@gmail.com", :subject => "Sandwich Request")
 	# make_sandwich@generalthings.com
   end
+
 end
